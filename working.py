3175d02b83c408ff777f6c37d9383bbb14591f7a
Oom Lout <aaron@oomlout.com>
2023-12-15 10:34:04 +0000

    working

--- working.py
+++ working.py
@@ -25,12 +25,12 @@ def main(**kwargs):
 
         
     
-    folder = kwargs.get("folder", f"os.path.dirname(__file__)/parts")
+    folder = kwargs.get("folder", f"{os.path.dirname(__file__)}/parts")
     folder = folder.replace("\\","/")
     
     kwargs["file_template_list"] = configuration
     print(f"oomlout_oomp_utility_readme_generation for folder: {folder}")
-    #create_recursive(**kwargs)
+    create_recursive(**kwargs)
     generate_navigation(**kwargs)
 
 
@@ -42,8 +42,15 @@ def generate_navigation(**kwargs):
     directory_list = glob.glob(f"{folder}/**/", recursive=True)
     
     # Create a dictionary to store categories and their corresponding items
-    for directory in directory_list:        
-        if not directory.endswith("part"):
+    for directory in directory_list:    
+        #if starts with a / remove it
+        if directory.startswith("/"):
+            directory = directory[1:]
+        directory = directory.replace("\\","/") 
+        directory_short = directory.replace(folder,"")  
+        if directory_short.startswith("/"):
+            directory_short = directory_short[1:] 
+        if not directory.endswith("part/"):
             current_directory_list = glob.glob(f"{directory}/**/", recursive=True)
             file_readme = os.path.join(directory, "readme.md")
             
@@ -55,7 +62,8 @@ def generate_navigation(**kwargs):
                 item = item.replace(directory_navigation_sanitized,"")
                 if item.endswith("part/"):
                     #remove leading /
-                    item = item[1:]
+                    if item.startswith("/"):
+                        item = item[1:]
                     current_directory_list_new.append(item)
             current_directory_list = current_directory_list_new
 
@@ -65,7 +73,9 @@ def generate_navigation(**kwargs):
                 file_paths = current_directory_list
                 directory_dict = {}
                 for file_path in file_paths:
-                    #print(file_path)                
+                    #print(file_path)   
+                    file_path = file_path.replace("\\","/")
+                    file_path = file_path.replace(directory_short,"")             
                     file_path_split = file_path.split("/")
                     file_path_split = file_path_split[:-2]
                     current_dict = directory_dict
@@ -75,13 +85,16 @@ def generate_navigation(**kwargs):
                         current_dict = current_dict[item]
                 
                 markdown_content = ""
+                markdown_content = generate_markdown(directory_dict)
+                pass
+                """
                 #display the directory_dict in a nested list            
                 for key, value in directory_dict.items():
                     current_link = f"{key}"
-                    markdown_content += f"* {key}\n"
+                    markdown_content += f"* [{key}]({current_link})\n"
                     for key2, value2 in value.items():
                         current_link += f"/{key2}"
-                        markdown_content += f"  * {key2}\n"
+                        markdown_content += f"  * [{key2}]({current_link})\n"
                         for key3, value3 in value2.items():
                             current_link += f"/{key3}"
                             markdown_content += f"    * [{key3}]({current_link})\n"
@@ -101,12 +114,23 @@ def generate_navigation(**kwargs):
                                                     markdown_content += f"                * [{key9}]({key9})\n"
                                                     for key10, value10 in value9.items():
                                                         markdown_content += f"                  * [{key10}]({key10})\n"
-                                                    
+                 """                                   
                 #write the markdown content to the readme
-                with open(file_readme, "w") as f:
-                    f.write(markdown_content)
+                if markdown_content != "":
+                    with open(file_readme, "w") as f:
+                        f.write(markdown_content)
                 
-
+def generate_markdown(directory_dict, current_link='', indent=0):
+    markdown_content = ''
+    for key, value in directory_dict.items():
+        markdown_content += '  ' * indent + f'* [{key}]({current_link}/{key})\n'
+        if isinstance(value, dict):
+            current_link_sanitized = current_link
+            if current_link_sanitized.startswith("/"):
+                current_link_sanitized = current_link_sanitized[1:]                
+            markdown_content += generate_markdown(value, f'{current_link_sanitized}/{key}', indent + 1)
+    
+    return markdown_content
             
 
 def create_recursive(**kwargs):
@@ -116,6 +140,7 @@ def create_recursive(**kwargs):
     kwargs["folder_template_absolute"] = folder_template_absolute
     for item in os.listdir(folder):
         item_absolute = os.path.join(folder, item)
+        item_absolute = item_absolute.replace("\\","/")
         if os.path.isdir(item_absolute):
             #if working.yaml exists in the folder
             if os.path.exists(os.path.join(item_absolute, "working.yaml")):
@@ -142,26 +167,31 @@ def generate(**kwargs):
         except yaml.YAMLError as exc:   
             print(exc)
 
-    folder_name = ""
-    folder_order = ["classification", "type", "color","description_main", "description_extra", "manufacturer","part_number"]
-    for item in folder_order:
-        if item in details:   
-            if details[item] != "":         
-                folder_name += f"/{details[item]}"
-    folder_name += f"/part"
-    folder_no_parts = folder.replace("/parts",f"/{folder_navigation}")
-    folder_name = f"{folder_no_parts}{folder_name}"
+    if details != None:
 
-    directory_source = directory
-    directory_destination = folder_name
+        folder_name = ""
+        folder_order = ["classification", "type", "size", "color","description_main", "description_extra", "manufacturer","part_number"]
+        for item in folder_order:
+            if item in details:   
+                if details[item] != "":         
+                    folder_name += f"/{details[item]}"
+        folder_name += f"/part"
+        folder_no_parts = folder.replace("/parts",f"/{folder_navigation}")
+        folder_no_parts = folder.replace("/things",f"/{folder_navigation}")
+        folder_name = f"{folder_no_parts}{folder_name}"
 
-    #copy the folder and create neccesary directories using xcopy for windows with overwite and cp for linux
-    if os.name == "nt":
-        command = f"xcopy \"{directory_source}\" \"{directory_destination}\" /e /i /y"
+        directory_source = directory
+        directory_destination = folder_name
+
+        #copy the folder and create neccesary directories using xcopy for windows with overwite and cp for linux
+        if os.name == "nt":
+            command = f"xcopy \"{directory_source}\" \"{directory_destination}\" /e /i /y"
+        else:
+            command = f"cp -r \"{directory_source}\" \"{directory_destination}\""
+        os.system(command)
+
     else:
-        command = f"cp -r \"{directory_source}\" \"{directory_destination}\""
-    os.system(command)
-    
+        print(f"no yaml file found in {directory}")    
 
 
 
@@ -171,7 +201,8 @@ if __name__ == '__main__':
     kwargs = {}
     folder = os.path.dirname(__file__)
     #folder = "C:/gh/oomlout_oomp_builder/parts"
-    folder = "C:/gh/oomlout_oomp_part_generation_version_1/parts"
+    #folder = "C:/gh/oomlout_oomp_part_generation_version_1/parts"
+    folder = "C:/gh/oomlout_oobb_version_4/things"
     kwargs["folder"] = folder
     overwrite = False
     kwargs["overwrite"] = overwrite
